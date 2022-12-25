866b6af435e0d6fdf61a5dd7565145374ea0bc65
Ahmed Nasr <ahmednasrhassan123@gmail.com>
2022-12-25 23:46:13 +0200

    Finish create table with checks

--- askForColumnsData.sh
+++ askForColumnsData.sh
@@ -1,19 +1,37 @@
 #!/usr/bin/bash
-echo "Add columns properties one by one (name datatype if pk enter pk)"
-
-select i in "Add Column" "Done";
+echo "Add columns properties one by one separated with spaces (name datatype if pk enter pk else don't enter anything)"
+pkCounter=0
+select i in "Add Column" "Done" "Delete";
 do
     case $i in
     "Add Column")
         read -p "Add Column data : " name datatype pk
-        echo "$pk -,$name,$datatype" >> ./$1
+        
+        if [ "$pk" = "pk" ] && [ "$pkCounter" -eq 0 ]; then
+            pkCounter=$((pkCounter + 1))
+            echo "$pk -,$name,$datatype" >> ./$1'_MetaData'
+        elif [ "$pk" != "pk" ]; then
+            echo "$pk -,$name,$datatype" >> ./$1'_MetaData'
+        else 
+            echo "Table should has one and only one pk column"
+        fi
+
     ;;
     "Done")
+        if [ "$pkCounter" -eq 0 ]; then
+            echo "Table should has one and only one pk column"
+        else 
+            break
+        fi
+    ;;
+    "Delete")
+        rm ./$1'_MetaData'
+        rm ./$1
         break
     ;;
     *)
     echo "Not valid option"
     esac
-    echo "1) Add Column  2) Done"
+    echo "1) Add Column  2) Done  3) Delete"
 done
 
